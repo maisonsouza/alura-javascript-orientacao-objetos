56a3340d229408720a610609ade2a4dbcfdeb333
Maison <maisongalvao144@gmail.com>
2020-05-11 17:44:48 -0400

    Aula01 - Repetição de código

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Curso de JavaScript: introdução a Orientação a Objetos
+![](https://www.alura.com.br/assets/api/share/curso-javascritpt-orientacao-objetos.png)
+
+> Aula01
+   - Repetição de código.
+   
+> Aula02
+   - Adicionando comportamentos.  
 
--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const cliente1Nome = "Maison";
-const cliente1CPF = 98225723287;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+// Nossa primeira classe
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
 
-const cliente2Nome = "Jose";
-const cliente2CPF = 88716074220;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+cliente1.nome = "Maison";
+cliente1.cpf = 98225723287;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Jose";
+cliente2.cpf = 88716074220;
+cliente2.agencia= 1001;
+cliente2.saldo = 0;
 
 const cliente3Nome = "Luciano";
-const cliente3CPF = 02271826268;
+const cliente3CPF = 02271
+826268;
 const cliente3Agencia = 1001;
 const cliente3Saldo = 0;
 
-// Problema da repetição de código
-
-console.log(cliente1Nome);
-console.log(cliente2Nome);
+console.log(cliente1, cliente2);
