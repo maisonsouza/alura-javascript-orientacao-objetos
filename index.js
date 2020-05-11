336fd6d35729fb4b3bf076e48507bafb1f54c9d2
Maison <maisongalvao144@gmail.com>
2020-05-11 17:58:33 -0400

    Comportamento de classes

--- index.js
+++ index.js
@@ -2,26 +2,37 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
 cliente1.nome = "Maison";
 cliente1.cpf = 98225723287;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Jose";
 cliente2.cpf = 88716074220;
-cliente2.agencia= 1001;
-cliente2.saldo = 0;
 
-const cliente3Nome = "Luciano";
-const cliente3CPF = 02271
-826268;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 120;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(50);
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(80);
+console.log(contaCorrenteRicardo.saldo);
 
-console.log(cliente1, cliente2);
+console.log(cliente1);
+console.log(cliente2)
