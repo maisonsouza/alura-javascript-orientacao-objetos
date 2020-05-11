08d37fec7cdc67d5b1058278adae73aac959a4f4
Maison <maisongalvao144@gmail.com>
2020-05-11 17:23:25 -0400

    Problema da repetição de código

---
+++ index.js
@@ -0,0 +1,19 @@
+const cliente1Nome = "Maison";
+const cliente1CPF = 98225723287;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Jose";
+const cliente2CPF = 88716074220;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+const cliente3Nome = "Luciano";
+const cliente3CPF = 02271826268;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+// Problema da repetição de código
+
+console.log(cliente1Nome);
+console.log(cliente2Nome);
